32dfdf598ea651af9e5d778a15b23b5d6a33dc00
Nicholas Allen <allen.2020@osu.edu>
2020-11-07 20:45:38 -0600

    Staff page redesign

--- views/views.py
+++ views/views.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 from datetime import date
 from django.shortcuts import render
@@ -9,30 +10,32 @@ from user.models import User
 
 def get_leaderboard(month, year):
     return ControllerSession.objects \
-            .filter(
-                start__year=year,
-                start__month=month
-            ) \
-            .values('user', 'user__first_name', 'user__last_name') \
-            .annotate(duration = Sum('duration')) \
-            .order_by('-duration')[0:5]
+        .filter(
+            start__year=year,
+            start__month=month
+        ) \
+        .values('user', 'user__first_name', 'user__last_name') \
+        .annotate(duration=Sum('duration')) \
+        .order_by('-duration')[0:5]
+
 
 def get_leaderboard_by_position(month, year, pos):
     return ControllerSession.objects \
-            .filter(
-                start__year=year,
-                start__month=month,
-                callsign__endswith=pos
-            ) \
-            .values('user', 'user__first_name', 'user__last_name') \
-            .annotate(duration = Sum('duration')) \
-            .order_by('-duration')[0:5]
+        .filter(
+            start__year=year,
+            start__month=month,
+            callsign__endswith=pos
+        ) \
+        .values('user', 'user__first_name', 'user__last_name') \
+        .annotate(duration=Sum('duration')) \
+        .order_by('-duration')[0:5]
 
 
 def view_home(request):
     online_controllers_count = Controller.objects.count()
     total_home_controllers = User.objects.filter(main_role='HC').count()
-    month_control_time = str(ControllerSession.objects.aggregate(Sum('duration'))['duration__sum']).split('.')[0]
+    month_control_time = str(ControllerSession.objects.aggregate(
+        Sum('duration'))['duration__sum']).split('.')[0]
     pilots = requests.get('https://api.denartcc.org/live/ZID').json()
 
     online_controllers = Controller.objects.all()
@@ -49,14 +52,16 @@ def view_home(request):
     twr_this_month = get_leaderboard_by_position(now.month, now.year, 'TWR')
 
     gnd_this_month = ControllerSession.objects \
-            .filter(
-                Q(start__year=now.year) & \
-                Q(start__month=now.month) & \
-                (Q(callsign__endswith='GND') | Q(callsign__endswith='DEL'))
-            ) \
-            .values('user', 'user__first_name', 'user__last_name') \
-            .annotate(duration = Sum('duration')) \
-            .order_by('-duration')[0:5]
+        .filter(
+            Q(start__year=now.year) &
+            Q(start__month=now.month) &
+            (Q(callsign__endswith='GND') | Q(callsign__endswith='DEL'))
+        ) \
+        .values('user', 'user__first_name', 'user__last_name') \
+        .annotate(duration=Sum('duration')) \
+        .order_by('-duration')[0:5]
+
+    dev_env = os.getenv('DEV_ENV')
 
     return render(request, 'home.html', {
         'page_title': 'Home',
@@ -71,5 +76,6 @@ def view_home(request):
         'ctr_this_month': ctr_this_month,
         'app_this_month': app_this_month,
         'twr_this_month': twr_this_month,
-        'gnd_this_month': gnd_this_month
+        'gnd_this_month': gnd_this_month,
+        'dev_env': dev_env
     })
