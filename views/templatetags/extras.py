957b87e698f96d7f13055d21c60eb98fa55c2321
Nicholas Allen <allen.2020@osu.edu>
2020-12-24 17:50:22 -0500

    Fix controlling time format bug

--- views/templatetags/extras.py
+++ views/templatetags/extras.py
@@ -18,9 +18,11 @@ def format_duration(td):
     to appear as colon-delimited.
     """
     time = re.findall(r'\d+', str(td))
-    hours = (int(time[0]) * 24) + int(time[1])
-    minutes = time[2]
-    return f'{hours}:{minutes}'
+    if len(time) == 5: # then there is a day value
+        result = f'{(int(time[0]) * 24) + int(time[1])}:{time[2]}'
+    else:
+        result = f'{time[0]}:{time[1]}'
+    return result
 
 
 @register.filter
