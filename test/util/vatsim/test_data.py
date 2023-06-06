90c71b16db1067eb9953efd7c59fb749f5374f90
nallen013 <allennick013@gmail.com>
2023-06-06 12:26:22 -0500

    Update configuration files

--- test/util/vatsim/test_data.py
+++ test/util/vatsim/test_data.py
@@ -1,26 +1,12 @@
 import pytest
 from requests_mock.mocker import Mocker
 
-from test.conftest import (
+# pylint: disable=wrong-import-order
+from test.helpers.conftest import (
     VATSIM_STATUS,
     VATSIM_DATA
 )
 from util.vatsim import data
-
-
-def mock_requests_get(*args, **kwargs):
-    class MockResponse:
-        def __init__(self, json_data, status_code):
-            self.json_data = json_data
-            self.status_code = status_code
-
-        def json(self):
-            return self.json_data
-
-    if args[0] == 'https://status.vatsim.net/status.json':
-        return MockResponse(VATSIM_STATUS, 200)
-
-    return MockResponse(None, 404)
 
 
 @pytest.mark.parametrize(
@@ -46,6 +32,7 @@ def test_get_data_url_error(requests_mock: Mocker):
     requests_mock.get('https://status.vatsim.net/status.json', json=VATSIM_STATUS)
     with pytest.raises(IndexError) as e_info:
         data.get_data_url('invalid_key')
+        assert e_info.value == 'Corresponding URL for invalid_key not found in VATSIM server status'
 
 
 def test_get_vatsim_data(requests_mock: Mocker):
